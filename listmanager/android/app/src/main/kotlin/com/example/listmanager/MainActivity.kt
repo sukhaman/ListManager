389837ecbcadf5def9bdc788b57e6c956e3aebc0
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-18 19:30:43 -0400

    NotesManager project renamed

--- listmanager/android/app/src/main/kotlin/com/example/listmanager/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.listmanager
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ notesmanager/android/app/src/main/kotlin/com/example/notesmanager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.decryptcode.notesmanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
