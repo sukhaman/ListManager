11c5c0a4e164668ca877f0329f1d9b95fd32ec67
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-24 11:38:18 -0400

    route for add new note added

--- notesmanager/lib/constans/routes.dart
+++ notesmanager/lib/constans/routes.dart
@@ -2,3 +2,4 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const homeRoute = '/homeview/';
 const verifyEmailRoute = '/verifyEmail/';
+const addNewNoteRoute = '/addNewNote/';
