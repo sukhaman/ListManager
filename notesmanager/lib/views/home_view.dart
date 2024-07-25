36cc4e76110ccbc9b3830b5ffd5c4e4a859297aa
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-25 12:53:31 -0400

    log out dialog added

--- notesmanager/lib/views/home_view.dart
+++ notesmanager/lib/views/home_view.dart
@@ -3,6 +3,8 @@ import 'package:notesmanager/constans/routes.dart';
 import 'package:notesmanager/enums/menu_action.dart';
 import 'package:notesmanager/services/auth/auth_service.dart';
 import 'package:notesmanager/services/crud/notes_service.dart';
+import 'package:notesmanager/utilities/dialogs/log_out_dialog.dart';
+import 'package:notesmanager/views/notes/notes_list_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -67,21 +69,11 @@ class _HomeViewState extends State<HomeView> {
                       case ConnectionState.waiting:
                         if (snapshot.hasData) {
                           final allNotes = snapshot.data as List<DatabaseNote>;
-
-                          return ListView.builder(
-                            itemCount: allNotes.length,
-                            itemBuilder: (context, index) {
-                              final note = allNotes[index];
-                              return ListTile(
-                                title: Text(
-                                  note.text,
-                                  maxLines: 1,
-                                  softWrap: true,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              );
-                            },
-                          );
+                          return NotesListView(
+                              notes: allNotes,
+                              onDeleteNote: (note) async {
+                                await _notesService.deleteNote(id: note.id);
+                              });
                         } else {
                           return const CircularProgressIndicator();
                         }
@@ -96,28 +88,4 @@ class _HomeViewState extends State<HomeView> {
       ),
     );
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure you want to sign out?'),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancel')),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-              child: const Text('Yes')),
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
 }
