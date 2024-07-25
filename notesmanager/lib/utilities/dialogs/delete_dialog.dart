de30a0e136567299106c986c0979ebd6e3afc6e6
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-25 13:14:06 -0400

    delete dialog added

---
+++ notesmanager/lib/utilities/dialogs/delete_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:notesmanager/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showDeleteDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+      context: context,
+      title: 'Delete',
+      content: 'Are you sure you want to delete this note?',
+      optionsBuilder: () => {'Cancel': false, 'Yes': true}).then(
+    (value) => value ?? false,
+  );
+}
