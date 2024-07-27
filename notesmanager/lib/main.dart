50ad1ba605f1a8b1b0e31efbe648bb0f6a093644
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-27 10:18:53 -0400

    AuthEventLogin method added to the main file

--- notesmanager/lib/main.dart
+++ notesmanager/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesmanager/constans/routes.dart';
-import 'package:notesmanager/services/auth/auth_service.dart';
+import 'package:notesmanager/services/auth/bloc/auth_State.dart';
+import 'package:notesmanager/services/auth/bloc/auth_bloc.dart';
+import 'package:notesmanager/services/auth/bloc/auth_event.dart';
+import 'package:notesmanager/services/auth/firebase_auth_provider.dart';
 import 'package:notesmanager/views/home_view.dart';
 import 'package:notesmanager/views/login_view.dart';
 import 'package:notesmanager/views/notes/create_update_note_view.dart';
@@ -23,7 +27,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainView(),
+      home: BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(FirebaseAuthProvider()),
+        child: const MainView(),
+      ),
       routes: {
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
@@ -40,24 +47,21 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: AuthService.firebase().initialize(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final currentUser = AuthService.firebase().currentUser;
-              if (currentUser != null) {
-                if (currentUser.isEmailVerified) {
-                  return const HomeView();
-                } else {
-                  return const VerifyEmailView();
-                }
-              } else {
-                return const LoginView();
-              }
-            default:
-              return const Text('Loading...');
-          }
-        });
+    context.read<AuthBloc>().add(const AuthEventIntialize());
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        if (state is AuthStateLoggedIn) {
+          return const HomeView();
+        } else if (state is AuthStateNeedsVerification) {
+          return const VerifyEmailView();
+        } else if (state is AuthStateLoggedOut) {
+          return const LoginView();
+        } else {
+          return const Scaffold(
+            body: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
