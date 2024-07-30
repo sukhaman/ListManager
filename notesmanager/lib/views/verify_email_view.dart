af783d1a17be4de9952f67c95c1dafe236f9b00d
Sukhaman Singh <sukhamandev@gmail.com>
2024-07-30 10:17:20 -0400

    padding added to the verify email view

--- notesmanager/lib/views/verify_email_view.dart
+++ notesmanager/lib/views/verify_email_view.dart
@@ -21,28 +21,31 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       appBar: AppBar(
         title: const Text('Verify Email'),
       ),
-      body: Column(
-        children: [
-          const Text(
-              "We've sent you an email verification. Please open it to verify your account."),
-          const Text(
-              "If you haven't received a verificaiton email yet, press the button below"),
-          TextButton(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text(
+                "We've sent you an email verification. Please open it to verify your account."),
+            const Text(
+                "If you haven't received a verificaiton email yet, press the button below"),
+            TextButton(
+                onPressed: () async {
+                  context.read<AuthBloc>().add(
+                        const AuthEventSendEmailVerificaiton(),
+                      );
+                },
+                child: const Text('Send email verification')),
+            TextButton(
               onPressed: () async {
                 context.read<AuthBloc>().add(
-                      const AuthEventSendEmailVerificaiton(),
+                      const AuthEventLogOut(),
                     );
               },
-              child: const Text('Send email verification')),
-          TextButton(
-            onPressed: () async {
-              context.read<AuthBloc>().add(
-                    const AuthEventLogOut(),
-                  );
-            },
-            child: const Text('Restart'),
-          )
-        ],
+              child: const Text('Restart'),
+            )
+          ],
+        ),
       ),
     );
   }
